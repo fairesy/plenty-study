a21abbec8223d47d25a5591766c75ac7837d0777
skychb <skyler@nhnnext.org>
2016-06-26 02:11:01 +0900

    list modified

--- src/dataStructure/List.java
+++ src/dataStructure/List.java
@@ -7,7 +7,7 @@ public class List {
         pitches.add("138");
         pitches.add("129");
         pitches.add("142");
-
+        
         System.out.println(pitches.get(1));
         System.out.println(pitches.size());
         System.out.println(pitches.contains("142"));
